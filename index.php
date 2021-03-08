521a6e2f7a013322204b6dec29d6b29129f9f492
dkharchi <djaouida.kharchi@gmail.com>
2021-03-08 09:08:34 -0500

    second commit

--- index.php
+++ index.php
@@ -2,4 +2,22 @@
 
 echo " Bonjour! c'est le projet GIT";
 
+echo "<h1>Test</h1>";
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet Git</title>
+</head>
+<body>
+<div>
+<p>
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda earum architecto vitae unde cumque atque. Harum magni aut earum dolor!
+</p></div>
+    
+</body>
+</html>
